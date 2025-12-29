a5a602fcd55827048d761382a4fa4adf5caeacd6
Matt DePillis <mattdepillis@gmail.com>
2025-12-28 21:33:15 -0500

    altered gitignore for xcode proj

---
+++ ios/HealthOS/HealthOS/HealthOS/HealthOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthOSApp.swift
+//  HealthOS
+//
+//  Created by Matt DePillis on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
